6d51b5d6016896702c5667db5ac4554e24412437
Teren Teh <tteh@oanda.com>
2012-01-11 10:10:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vine::Application.config.secret_token = '01af57a3d57154e36d0dfa21cab76f0820c9fda0dcb9acf13a0f8facabc94278fb2ac4cc3d88d0e654add1fbbcd74a45813cab5c33e27ecfa286069f7c80bb49'
